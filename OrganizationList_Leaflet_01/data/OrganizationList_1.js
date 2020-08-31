b1d89452b510275fe32d19567b53d2073f283532
xgloria <67011219+xgloria@users.noreply.github.com>
2020-08-31 12:41:07 -0500

    leaflet_01

---
+++ OrganizationList_Leaflet_01/data/OrganizationList_1.js
@@ -0,0 +1,266 @@
+var json_OrganizationList_1 = {
+    "type": "FeatureCollection",
+    "name": "OrganizationList_1",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M001",
+            "MUSEUM ABBREV.": "AIC",
+            "MUSEUM": "Art Institute of Chicago",
+            "LONGITUDE": -87.62388888,
+            "LATITUDE": 41.87944445,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-87.62388888, 41.87944445]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M002",
+            "MUSEUM ABBREV.": "BM",
+            "MUSEUM": "British Museum",
+            "LONGITUDE": -0.1269,
+            "LATITUDE": 51.5195,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-0.1269, 51.5195]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M003",
+            "MUSEUM ABBREV.": "CMA",
+            "MUSEUM": "Cleveland Museum of Art",
+            "LONGITUDE": -81.6116667,
+            "LATITUDE": 41.50888888,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-81.6116667, 41.50888888]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M004",
+            "MUSEUM ABBREV.": "DMIA",
+            "MUSEUM": "Doha Museum of Islamic Art",
+            "LONGITUDE": 51.53916667,
+            "LATITUDE": 25.29527778,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [51.53916667, 25.29527778]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M005",
+            "MUSEUM ABBREV.": "DPM",
+            "MUSEUM": "Palace Museum, Beijing",
+            "LONGITUDE": 116.39077,
+            "LATITUDE": 39.91708,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [116.39077, 39.91708]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M006",
+            "MUSEUM ABBREV.": "HAM",
+            "MUSEUM": "Harvard Art Museum",
+            "LONGITUDE": -71.1147,
+            "LATITUDE": 42.3742,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-71.1147, 42.3742]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M007",
+            "MUSEUM ABBREV.": "MET",
+            "MUSEUM": "Metropolitan Museum of Art (NYC)",
+            "LONGITUDE": -73.9631,
+            "LATITUDE": 40.7794,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-73.9631, 40.7794]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M008",
+            "MUSEUM ABBREV.": "NMAA",
+            "MUSEUM": "National Museum of Asian Art (Freer Sackler Gallery )",
+            "LONGITUDE": -77.02739,
+            "LATITUDE": 38.888135,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-77.02739, 38.888135]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M009",
+            "MUSEUM ABBREV.": "NPMT",
+            "MUSEUM": "National Palace Museum, Taipei",
+            "LONGITUDE": 121.5486111,
+            "LATITUDE": 25.10194445,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [121.5486111, 25.10194445]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M010",
+            "MUSEUM ABBREV.": "PMA",
+            "MUSEUM": "Philadelphia Museum of Art",
+            "LONGITUDE": -75.181,
+            "LATITUDE": 39.996,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-75.181, 39.996]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M011",
+            "MUSEUM ABBREV.": "SAAM",
+            "MUSEUM": "Seattle Asian Art Museum",
+            "LONGITUDE": -122.3142337,
+            "LATITUDE": 47.6302814,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-122.3142337, 47.6302814]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M012",
+            "MUSEUM ABBREV.": "SFAAM",
+            "MUSEUM": "San Francisco Asian Art Museum",
+            "LONGITUDE": -122.416577,
+            "LATITUDE": 37.780276,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-122.416577, 37.780276]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M013",
+            "MUSEUM ABBREV.": "VAM ",
+            "MUSEUM": "Victoria and Albert Museum",
+            "LONGITUDE": -1.67194445,
+            "LATITUDE": 51.49638888,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-1.67194445, 51.49638888]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M014",
+            "MUSEUM ABBREV.": "MR",
+            "MUSEUM": "Museum Rietberg, Zurich",
+            "LONGITUDE": 8.530225,
+            "LATITUDE": 47.3589294,
+            "Note": "Museum collection ",
+            "Last Seen Date": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [8.530225, 47.3589294]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M015",
+            "MUSEUM ABBREV.": "Sotheby's HK",
+            "MUSEUM": "Sotheby's Hong Kong",
+            "LONGITUDE": 114.1653211,
+            "LATITUDE": 22.276794,
+            "Note": "Auction House appearance",
+            "Last Seen Date": "Last seen 25 April 2004"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [114.1653211, 22.276794]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M016",
+            "MUSEUM ABBREV.": "Christie's NY",
+            "MUSEUM": "Christie's New York",
+            "LONGITUDE": -73.980023,
+            "LATITUDE": 40.758575,
+            "Note": "Auction House appearance",
+            "Last Seen Date": "Last seen 19 June 2019"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-73.980023, 40.758575]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "PLACE ID": "M017",
+            "MUSEUM ABBREV.": "Christie's LDN",
+            "MUSEUM": "Christie's London",
+            "LONGITUDE": -0.137368,
+            "LATITUDE": 51.506573,
+            "Note": "Auction House appearance",
+            "Last Seen Date": "Last seen 11 November 2003"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-0.137368, 51.506573]
+        }
+    }]
+}
